9cd7d183cebca5393f83c5c581ca26a82df9b3da
struiner <yurivaillant@gmail.com>
2018-09-11 16:25:16 +0200

    Update dialog.component.ts

--- example/src/app/shared/components/popup/dialog/dialog.component.ts
+++ example/src/app/shared/components/popup/dialog/dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dialog',
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogComponent implements OnInit {
 
+  @Input()
+  public title: string;
+  
+  @Input()
+  public message:string;
+  
+  @Input()
+  public options:Array<DialogOption>;
+  
+  
   constructor() { }
 
   ngOnInit() {
